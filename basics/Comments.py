9dfb895c7f9cd4156e3686dea00080ba42c7291b
msanupama <msdanupama@gmail.com>
2021-06-24 16:23:22 +0530

    Create Comments.py

---
+++ basics/Comments.py
@@ -0,0 +1,23 @@
+#A Comment starts with a hash(#) symbol. Anything written after hash symbol is not displayed in the output in Python.
+
+#This is a single line comment
+
+#This is a multiline comment.
+#A multiline comment consists of
+#more then one single line comments
+#which are preceded by a hash symbol.
+
+"""
+A document string is also not interpreted by the interpretor just as it does for comments.
+A doc string is written within 3 pairs of double quotes/single quotes in Python.
+A doc string can be used as a heading in Python. It can also be used as a comment"""
+
+""" A document String is also a type
+of comment which can be used as a heading
+in Python Programming."""
+
+print("Hey! Welcome to Python Idle")
+
+'''This is
+a doc string'''
+
