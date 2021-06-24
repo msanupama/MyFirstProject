d285ab86d5d823004d6a6a7b18bc2b377d87b7f1
msanupama <msdanupama@gmail.com>
2021-06-24 13:58:36 +0530

    Update create_a_class.py

--- basics/create_a_class.py
+++ basics/create_a_class.py
@@ -1,9 +1,9 @@
-class System:
+class System:  #Creating a Class
     def begin(self):
         print("1 2 3 4 5")
 
-s1=System()
+s1=System() #Creating an object
 
-##System.begin(s1)
+System.begin(s1) #Calling the function in the class with Class name.
 
-s1.begin()
+s1.begin() #Calling the function with Object of the same Class.
