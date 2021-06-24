a3ed275450ce6961fbbce7e4523f6c5c624e6a2a
msanupama <msdanupama@gmail.com>
2021-06-24 16:23:35 +0530

    Create print_my_name.py

---
+++ basics/print_my_name.py
@@ -0,0 +1,11 @@
+class MyName: #Class Declaration.
+    def pname(self): #Class method Declaration.
+        print("My name is :",name)
+
+name= "Anupama"
+
+p1=MyName()             #Class Object Declaration.
+
+MyName.pname(p1)         #Calling the Function using Class name.
+
+p1.pname()                #Calling the Function using Class Object.
