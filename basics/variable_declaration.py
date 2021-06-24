70f927693d18b3d828bb47fb43ea702e3765a3f8
msanupama <msdanupama@gmail.com>
2021-06-24 16:23:43 +0530

    Create variable_declaration.py

---
+++ basics/variable_declaration.py
@@ -0,0 +1,14 @@
+x=2
+print("Value of x is :",x," and the data type of x is :",type(x))
+
+x=7.5
+print("Value of x is :",x," and the data type of x is :",type(x))
+
+x="Python"
+print("Value of x is :",x," and the data type of x is :",type(x))
+
+x=True
+print("Value of x is :",x," and the data type of x is :",type(x))
+
+x=4+2j
+print("Value of x is :",x," and the data type of x is :",type(x))
