a5a148f3357a5043fd17db012782eb95de926a37
msanupama <msdanupama@gmail.com>
2021-07-03 16:16:40 +0530

    Create largest_of_3.py

---
+++ Loops/largest_of_3.py
@@ -0,0 +1,14 @@
+#Find the largest of the 3 numbers.
+a=20
+b=30
+c=40
+large=0
+if a>b and a>c:
+    large=a
+elif b>a and b>c:
+    large=b
+else:
+    large=c
+print("The largest of the 3 numbers is : ",large)
+        
+    
