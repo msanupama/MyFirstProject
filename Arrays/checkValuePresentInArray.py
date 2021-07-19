29cad61d5983b1633280ec0442f6ff2d919dcf0d
msanupama <msdanupama@gmail.com>
2021-07-19 15:59:04 +0530

    Create checkValuePresentInArray.py

---
+++ Arrays/checkValuePresentInArray.py
@@ -0,0 +1,11 @@
+#Let's write a function to check if a specific value exists in an array
+
+import array as arr
+
+def exists(n):       #function to check the presence of a number
+    return n in a     #returns True or False
+
+
+n=4
+a=arr.array('i',[1,2,3,4,5,6,7,8])
+print("The number ",n, " exists in the array T/F: ",exists(n))
