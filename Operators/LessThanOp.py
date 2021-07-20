fb77fba83c5257b93ad848d7f401b441438cb45d
msanupama <msdanupama@gmail.com>
2021-07-20 15:50:40 +0530

    Create LessThanOp.py

---
+++ Operators/LessThanOp.py
@@ -0,0 +1,15 @@
+#Let's write program for Relational Operators
+# '<''>''<=''>='
+
+#Less than Operator '<'
+
+#Let's write a program to print all the numbers 'less than'
+#10
+
+n=0
+
+while n<10:
+    print(n)
+    n+=1
+    
+    
