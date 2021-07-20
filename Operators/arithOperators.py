59fa5a71bb6db2dbd716f7edf907b928108dddca
msanupama <msdanupama@gmail.com>
2021-07-20 15:50:09 +0530

    Create arithOperators.py

---
+++ Operators/arithOperators.py
@@ -0,0 +1,18 @@
+#Let's write functions for Arithematic operators(+,-,/,*)
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+print("The sum of the 2 numbers is :",add(20,10))
+print("The difference of the 2 numbers is :",sub(20,10))
+print("The product of the 2 numbers is :",mul(20,10))
+print("The division of the 2 numbers is :",div(20,10))
