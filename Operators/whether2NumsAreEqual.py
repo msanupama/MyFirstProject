6fe9f1e061a004af4d031babd8150cf0ea609aed
msanupama <msdanupama@gmail.com>
2021-07-20 15:50:24 +0530

    Create whether2NumsAreEqual.py

---
+++ Operators/whether2NumsAreEqual.py
@@ -0,0 +1,9 @@
+#Let's write a program to determine whether the two given numbers are equal
+
+m=45
+n=45
+
+if m==n:
+    print("The 2 numbers are equal.")
+else:
+    print("The 2 numbers are not equal.")
