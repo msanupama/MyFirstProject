d13dc847cbb5e9bb74be61abd1d5f259d892372d
msanupama <msdanupama@gmail.com>
2021-07-20 15:50:27 +0530

    Create smallerLarger.py

---
+++ Operators/smallerLarger.py
@@ -0,0 +1,9 @@
+#Let's write a program to print larger and smaller number
+
+n=85
+m=75
+
+if n<m:
+    print(n, " is smaller and ",m, " is larger.")
+else:
+    print(m, " is smaller and ",n, " is larger.")
