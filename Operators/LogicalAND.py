9800e5adc930fc9b0824df81e91f36e9664e715a
msanupama <msdanupama@gmail.com>
2021-07-20 15:50:36 +0530

    Create LogicalAND.py

---
+++ Operators/LogicalAND.py
@@ -0,0 +1,16 @@
+#Let's write program to illustrate LOGICAL AND
+
+#Let's write a program to determine the smallest of 3 numbers
+
+a=75
+b=40
+c=15
+smallest=0
+if a<b and a<c:
+    smallest=a
+elif b<a and b<c:
+    smallest=b
+else:
+    smallest=c
+print("THe smallest number is :", smallest)
+
