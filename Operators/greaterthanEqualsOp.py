611ee3ec484c3884a00ed4dc69f682f8e46ea5aa
msanupama <msdanupama@gmail.com>
2021-07-20 15:50:17 +0530

    Create greaterthanEqualsOp.py

---
+++ Operators/greaterthanEqualsOp.py
@@ -0,0 +1,11 @@
+#Let's write a program to illustrate '>=' operator
+
+#We will print all numbers 'greater than' or equal to 4 upto 10
+
+n=0
+while n<=10:
+    if n>=4:
+        print(n)
+    else:
+        n+=1
+    n+=1
