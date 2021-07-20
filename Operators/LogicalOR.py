43e7e7377226040152d48fc9071a174c394fa080
msanupama <msdanupama@gmail.com>
2021-07-20 15:50:30 +0530

    Create LogicalOR.py

---
+++ Operators/LogicalOR.py
@@ -0,0 +1,11 @@
+#Let's write a program to illustrate LOGICAL OR
+
+#LOGICAL OR - If one of the inputs is TRUE, the output will be TRUE
+
+n='b'
+m='a'
+
+if type(n)==int or type(m)==int:
+    print("One of them is an integer.")
+else:
+    print("Both of them are not integers.")
