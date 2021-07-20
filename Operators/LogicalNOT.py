fef1db0a8813041b5ab8d307aa060102214bb0ab
msanupama <msdanupama@gmail.com>
2021-07-20 15:50:33 +0530

    Create LogicalNOT.py

---
+++ Operators/LogicalNOT.py
@@ -0,0 +1,19 @@
+#Let's write a program to illustrate Logical NOT
+
+#LOGICAL NOT - It is used to negate the output
+
+#For eg: Let us write a program to print FALSE for a TRUE result
+#using LOGICAL NOT
+
+print(not True)     #prints False
+print(not False)     #prints True
+print(not 0)        #prints True
+print(not 1)        #prints False
+
+
+n=0
+if not n==True:       # n=0...
+                       #not 0 = True
+    print("The number is 0.")
+
+ 
