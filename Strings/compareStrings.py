4c4a9c87e42103835e9c96bc168784f96ee989f6
msanupama <msdanupama@gmail.com>
2021-07-23 15:41:03 +0530

    Create compareStrings.py

---
+++ Strings/compareStrings.py
@@ -0,0 +1,9 @@
+#Let's write a program to compare strings using '=' operator.
+
+my_str1="Hello!"
+my_str2="Hai!"
+
+if my_str1==my_str2:
+    print("The 2 strings are equal.")
+else:
+    print("The 2 strings are not equal.")
