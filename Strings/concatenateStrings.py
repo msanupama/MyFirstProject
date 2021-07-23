d2d24f8f493d439e7eb9f98a6b143e4c118c8566
msanupama <msdanupama@gmail.com>
2021-07-23 15:40:59 +0530

    Create concatenateStrings.py

---
+++ Strings/concatenateStrings.py
@@ -0,0 +1,8 @@
+#Let's write a program to concatenate 2 strings
+
+my_str1="Hello "
+print(my_str1)
+my_str2="World!"
+print(my_str2)
+my_str3=my_str1+my_str2   #String Concatenation using '+' symbol
+print(my_str3)
