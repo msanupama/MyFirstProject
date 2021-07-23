4049e7b9febed3557180d3b9d834a48988d40b2e
msanupama <msdanupama@gmail.com>
2021-07-23 15:40:23 +0530

    Create subString.py

---
+++ Strings/subString.py
@@ -0,0 +1,10 @@
+#Let's write a program to extract a substring from a string
+
+my_str="Hello world!"
+
+
+sub_str=my_str[2:9]    #Extracts the substring
+print(sub_str)         #PRints the substring
+
+
+
