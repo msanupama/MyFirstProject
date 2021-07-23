278b8e960ab51d0c80eae7400b354d5294d4cbd7
msanupama <msdanupama@gmail.com>
2021-07-23 15:40:40 +0530

    Create equalsIgnoreCase.py

---
+++ Strings/equalsIgnoreCase.py
@@ -0,0 +1,10 @@
+#To compare 2 strings irrespective of whether they are in
+#uppercase or lowercase
+
+s1="Hai HeLLO"
+s2="Hai hello"
+
+if s1.lower()== s2.lower():
+    print("Both strings are equal")
+else:
+    print("Both strings are not equal")
