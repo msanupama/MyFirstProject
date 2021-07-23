06b5e5780ada43c671308ce26504308806fc9b27
msanupama <msdanupama@gmail.com>
2021-07-23 15:39:53 +0530

    Create startswithfunc.py

---
+++ Strings/startswithfunc.py
@@ -0,0 +1,13 @@
+#To illustrate startswith()
+
+s1="Hello, Good Evening!"
+
+res=s1.startswith("Hello")
+
+print(res)     #prints True if the string starts with the specified value.
+
+print('*'*50)
+
+res=s1.startswith("Evening")
+
+print(res)  
