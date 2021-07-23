66227c33e58cfe7ea3b27a98d3d182e021b9d1e4
msanupama <msdanupama@gmail.com>
2021-07-23 15:40:52 +0530

    Create DiffStrings.py

---
+++ Strings/DiffStrings.py
@@ -0,0 +1,31 @@
+#Let's write a program to show different ways of creating a
+# string in Python
+
+#First Method : With a pair of single quotes
+
+my_str='Hello'
+print(my_str)
+
+#Second Method : With a pair of double quotes
+
+my_str="Hello"
+print(my_str)
+
+#Third MEthod : With a pair of triple quotes
+
+my_str='''Hello'''
+print(my_str)
+
+#Fourth Method : With 3 pairs of double quotes
+#This method is generally used to create docstrings for
+#documentation purposes.This method is used to create
+#multiline strings at a time.
+
+my_str="""Hello"""
+print(my_str)
+my_str1="""Hello!
+This is a Python program. This program illustrates
+different ways of creating a String.
+               Thank you."""
+
+print(my_str1)
