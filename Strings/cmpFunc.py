ccda3810e057097f3a5c4bb50bad1f410dc7115f
msanupama <msdanupama@gmail.com>
2021-07-23 15:39:33 +0530

    Create cmpFunc.py

---
+++ Strings/cmpFunc.py
@@ -0,0 +1,11 @@
+#To illustrate cmp() function to compare to strings
+# In cmp(a,b):
+#if a>b o/p will be 1
+#if a=b o/p will be 0
+#if a<b o/p will be -1
+
+
+
+print cmp('abc','def')
+print cmp('abc','abc')
+print cmp('def','abc')
