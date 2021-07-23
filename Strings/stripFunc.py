cd6c11ce5320ef32037c0f13202f6ee56af58c27
msanupama <msdanupama@gmail.com>
2021-07-23 15:39:56 +0530

    Create stripFunc.py

---
+++ Strings/stripFunc.py
@@ -0,0 +1,14 @@
+#To illustrate String trimming equivalent Strip() function in Python.
+
+#Strip() function removes the whitespaces from the specified string
+#including whitespaces.
+
+#lstrip() function removes whitespaces to the left of the specified String.
+
+#rstrip() function removes whitespaces to the right of the specified String.
+
+s1= '   Hi   '
+
+print(s1.strip())    #strips whitespaces on both left and right side of the string.
+print(s1.lstrip())   #strips whitespaces from only the left side of the string.
+print(s1.rstrip())    #strips whitespaces from only the right side of the string.
